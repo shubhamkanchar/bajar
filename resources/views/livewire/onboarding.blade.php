107f73d12d612c3406bda46e207938d726565dcd
shubhamkanchar <shubhamkanchar687@gmail.com>
2025-03-09 22:59:29 +0530

    on board

--- resources/views/livewire/onboarding.blade.php
+++ resources/views/livewire/onboarding.blade.php
@@ -1,7 +1,7 @@
 <div class="card rounded-4 shadow">
 
     <div class="row p-4">
-        <div class="offset-xl-2 offset-lg-1 col-xl-4 col-lg-6 col-md-6 col-sm-12 p-4">
+        <div class="offset-xl-2 offset-lg-1 col-xl-5 col-lg-7 col-md-6 col-sm-12 p-4">
             @if ($step === 1)
                 <div class="row">
                     <div class="col-6">
@@ -354,137 +354,83 @@
                         <img class="mb-5" src="{{ asset('assets/image/business/step_3.png') }}">
                     </div>
                 </div>
-                <h6 class="text-muted fst-italic mb-3">"Awesome! Share some quick info to get started."</h6>
-                <h5 class="fw-bold mb-4">Business Details</h5>
-                <div class="row mb-3">
-                    <!-- Business Name -->
-                    <div class="col-12 col-md-6 mb-3 ">
-                        <div class="form-floating">
-                            <input type="text" id="business_name" class="form-control" wire:model="business_name"
-                                placeholder="Please enter your business name">
-                            <label for="business_name">Business Name *</label>
-                        </div>
-                        @error('business_name')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-
-                    <!-- GST Number -->
-                    <div class="col-12 col-md-6 mb-3">
-                        <div class="form-floating">
-                            <input type="number" id="gst_number" class="form-control" wire:model="gst_number"
-                                placeholder="Please enter your GST number">
-                            <label for="gst_number">GST Number</label>
-                        </div>
-                        @error('gst_number')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class="row mb-5">
-                    <!-- Phone Number -->
-                    <div class="col-12 col-md-6 mb-3">
-                        <div class="form-floating">
-                            <input type="text" id="phone" class="form-control" wire:model="phone"
-                                placeholder="Please enter your phone number">
-                            <label for="phone">Phone Number *</label>
-                        </div>
-                        @error('phone')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-
-                    <!-- Email -->
-                    <div class="col-12 col-md-6 mb-3">
-                        <div class="form-floating">
-                            <input type="email" id="email" class="form-control" wire:model="email"
-                                placeholder="Please enter your email">
-                            <label for="email">Email ID *</label>
-                        </div>
-                        @error('email')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-
-                <h5 class="fw-bold mb-4">Business Address</h5>
-
+                <h6 class="text-muted fst-italic mb-3">"Great! Share some more about your business"</h6>
+                <h5 class="fw-bold mb-4">Business Offerings</h5>
                 <div class="row">
-                    <!-- Business Address -->
-                    <div class="col-12 mb-3">
-                        <div class="form-floating">
-                            <input type="text" id="business_address" class="form-control"
-                                wire:model="business_address" placeholder="Please enter your business address">
-                            <label for="business_address">Business Address</label>
+                    <div class="col-lg-6 col-md-12 mb-2">
+                        <div class="width-100 border border-2 rounded-2 p-4">
+                            <span class="me-2">
+                                <svg width="22" height="18" viewBox="0 0 22 18" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M20.0235 8.05V16C20.0235 16.55 19.8277 17.0208 19.436 17.4125C19.0443 17.8042 18.5735 18 18.0235 18H4.0235C3.4735 18 3.00267 17.8042 2.611 17.4125C2.21933 17.0208 2.0235 16.55 2.0235 16V8.05C1.64017 7.7 1.34433 7.25 1.136 6.7C0.927668 6.15 0.923501 5.55 1.1235 4.9L2.1735 1.5C2.30683 1.06667 2.54433 0.708333 2.886 0.425C3.22767 0.141667 3.6235 0 4.0735 0H17.9735C18.4235 0 18.8152 0.1375 19.1485 0.4125C19.4818 0.6875 19.7235 1.05 19.8735 1.5L20.9235 4.9C21.1235 5.55 21.1193 6.14167 20.911 6.675C20.7027 7.20833 20.4068 7.66667 20.0235 8.05ZM13.2235 7C13.6735 7 14.0152 6.84583 14.2485 6.5375C14.4818 6.22917 14.5735 5.88333 14.5235 5.5L13.9735 2H12.0235V5.7C12.0235 6.05 12.1402 6.35417 12.3735 6.6125C12.6068 6.87083 12.8902 7 13.2235 7ZM8.7235 7C9.10684 7 9.41934 6.87083 9.661 6.6125C9.90267 6.35417 10.0235 6.05 10.0235 5.7V2H8.0735L7.5235 5.5C7.45683 5.9 7.54433 6.25 7.786 6.55C8.02767 6.85 8.34017 7 8.7235 7ZM4.2735 7C4.5735 7 4.836 6.89167 5.061 6.675C5.286 6.45833 5.4235 6.18333 5.4735 5.85L6.0235 2H4.0735L3.0735 5.35C2.9735 5.68333 3.02767 6.04167 3.236 6.425C3.44433 6.80833 3.79017 7 4.2735 7ZM17.7735 7C18.2568 7 18.6068 6.80833 18.8235 6.425C19.0402 6.04167 19.0902 5.68333 18.9735 5.35L17.9235 2H16.0235L16.5735 5.85C16.6235 6.18333 16.761 6.45833 16.986 6.675C17.211 6.89167 17.4735 7 17.7735 7ZM4.0235 16H18.0235V8.95C17.9402 8.98333 17.886 9 17.861 9H17.7735C17.3235 9 16.9277 8.925 16.586 8.775C16.2443 8.625 15.9068 8.38333 15.5735 8.05C15.2735 8.35 14.9318 8.58333 14.5485 8.75C14.1652 8.91667 13.7568 9 13.3235 9C12.8735 9 12.4527 8.91667 12.061 8.75C11.6693 8.58333 11.3235 8.35 11.0235 8.05C10.7402 8.35 10.411 8.58333 10.036 8.75C9.661 8.91667 9.25683 9 8.8235 9C8.34017 9 7.90267 8.91667 7.511 8.75C7.11934 8.58333 6.7735 8.35 6.4735 8.05C6.1235 8.4 5.77767 8.64583 5.436 8.7875C5.09433 8.92917 4.70683 9 4.2735 9H4.161C4.11933 9 4.0735 8.98333 4.0235 8.95V16Z"
+                                        fill="black" />
+                                </svg>
+                            </span>
+                            <span>
+                                <span>I sell,</span>
+                                <span class="fw-bold fs-6">Building Material</span>
+                            </span>
+                            <span class="float-end">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_737_3347)">
+                                        <circle cx="12" cy="12" r="12" fill="black" />
+                                        <path
+                                            d="M9.75049 14.0613L16.5811 7.23293C16.7363 7.07764 16.9337 7 17.1733 7C17.4131 7 17.6108 7.07746 17.7666 7.23237C17.9222 7.38729 18 7.5844 18 7.82369C18 8.06318 17.9222 8.26066 17.7666 8.41613L10.4622 15.6955C10.2588 15.8985 10.0216 16 9.75049 16C9.47943 16 9.2422 15.8985 9.0388 15.6955L6.23339 12.9065C6.0778 12.7515 6 12.5545 6 12.3154C6 12.0761 6.07761 11.8787 6.23282 11.7233C6.38804 11.568 6.58553 11.4903 6.82529 11.4903C7.06524 11.4903 7.2631 11.568 7.41888 11.7233L9.75049 14.0613Z"
+                                            fill="white" />
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_737_3347">
+                                            <rect width="24" height="24" fill="white" />
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+
+                            </span>
                         </div>
-                        @error('business_address')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
                     </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="width-100 border border-2 rounded-2 p-4">
+                            <span class="me-2">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M6.8 8.95348L8.95 6.77848L7.55 5.35348L7.15 5.75348C6.96667 5.93682 6.7375 6.03265 6.4625 6.04098C6.1875 6.04932 5.95 5.95348 5.75 5.75348C5.55 5.55348 5.45 5.31598 5.45 5.04098C5.45 4.76598 5.55 4.52848 5.75 4.32848L6.125 3.95348L5 2.82848L2.825 5.00348L6.8 8.95348ZM15 17.1785L17.175 15.0035L16.05 13.8785L15.65 14.2535C15.45 14.4535 15.2167 14.5535 14.95 14.5535C14.6833 14.5535 14.45 14.4535 14.25 14.2535C14.05 14.0535 13.95 13.8201 13.95 13.5535C13.95 13.2868 14.05 13.0535 14.25 12.8535L14.625 12.4535L13.2 11.0535L11.05 13.2035L15 17.1785ZM2 19.0035C1.71667 19.0035 1.47917 18.9076 1.2875 18.716C1.09583 18.5243 1 18.2868 1 18.0035V15.1785C1 15.0451 1.025 14.916 1.075 14.791C1.125 14.666 1.2 14.5535 1.3 14.4535L5.375 10.3785L1.05 6.05348C0.766667 5.77015 0.625 5.42015 0.625 5.00348C0.625 4.58682 0.766667 4.23682 1.05 3.95348L3.95 1.05348C4.23333 0.770149 4.58333 0.632649 5 0.640982C5.41667 0.649315 5.76667 0.795149 6.05 1.07848L10.4 5.40348L14.175 1.60348C14.375 1.40348 14.6 1.25348 14.85 1.15348C15.1 1.05348 15.3583 1.00348 15.625 1.00348C15.8917 1.00348 16.15 1.05348 16.4 1.15348C16.65 1.25348 16.875 1.40348 17.075 1.60348L18.4 2.95348C18.6 3.15348 18.75 3.37848 18.85 3.62848C18.95 3.87848 19 4.13682 19 4.40348C19 4.67015 18.95 4.92432 18.85 5.16598C18.75 5.40765 18.6 5.62848 18.4 5.82848L14.625 9.62848L18.95 13.9535C19.2333 14.2368 19.375 14.5868 19.375 15.0035C19.375 15.4201 19.2333 15.7701 18.95 16.0535L16.05 18.9535C15.7667 19.2368 15.4167 19.3785 15 19.3785C14.5833 19.3785 14.2333 19.2368 13.95 18.9535L9.625 14.6285L5.55 18.7035C5.45 18.8035 5.3375 18.8785 5.2125 18.9285C5.0875 18.9785 4.95833 19.0035 4.825 19.0035H2ZM3 17.0035H4.4L14.2 7.22848L12.775 5.80348L3 15.6035V17.0035ZM13.5 6.52848L12.775 5.80348L14.2 7.22848L13.5 6.52848Z"
+                                        fill="black" />
+                                </svg>
 
-                    <!-- State Dropdown -->
-                    <div class="col-12 col-md-6 mb-3 ">
-                        <div class="form-floating">
-                            <select class="form-select" id="state" wire:model="state">
-                                <option value="">Select State</option>
-                                @foreach ($states as $stateOption)
-                                    <option value="{{ $stateOption }}">{{ $stateOption }}</option>
-                                @endforeach
-                            </select>
-                            <label for="state">State</label>
+                            </span>
+                            <span>
+                                <span>I offer,</span>
+                                <span class="fw-bold fs-6">Services</span>
+                            </span>
+                            <span class="float-end">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_737_3347)">
+                                        <circle cx="12" cy="12" r="12" fill="black" />
+                                        <path
+                                            d="M9.75049 14.0613L16.5811 7.23293C16.7363 7.07764 16.9337 7 17.1733 7C17.4131 7 17.6108 7.07746 17.7666 7.23237C17.9222 7.38729 18 7.5844 18 7.82369C18 8.06318 17.9222 8.26066 17.7666 8.41613L10.4622 15.6955C10.2588 15.8985 10.0216 16 9.75049 16C9.47943 16 9.2422 15.8985 9.0388 15.6955L6.23339 12.9065C6.0778 12.7515 6 12.5545 6 12.3154C6 12.0761 6.07761 11.8787 6.23282 11.7233C6.38804 11.568 6.58553 11.4903 6.82529 11.4903C7.06524 11.4903 7.2631 11.568 7.41888 11.7233L9.75049 14.0613Z"
+                                            fill="white" />
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_737_3347">
+                                            <rect width="24" height="24" fill="white" />
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+
+                            </span>
                         </div>
-                        @error('state')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
                     </div>
-
-                    <!-- City Dropdown -->
-                    <div class="col-12 col-md-6 mb-3">
-                        <div class="form-floating">
-                            <select class="form-select" id="city" wire:model="city">
-                                <option value="">Select City</option>
-                                @foreach ($cities as $cityOption)
-                                    <option value="{{ $cityOption }}">{{ $cityOption }}</option>
-                                @endforeach
-                            </select>
-                            <label for="city">City</label>
-                        </div>
-                        @error('city')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
+                    <div class="col-12">
+                        <h4 class="fw-bold mt-3">Categories i deal in (00/03)</h4>
+                        <span class="badge rounded-pill text-dark border border-2 fs-6 p-3 m-1">Tiles & Granites</span>
+                        <span class="badge rounded-pill text-bg-light fs-6 p-3 m-1">Tiles & Granites</span>
+                        <span class="badge rounded-pill text-bg-light fs-6 p-3 m-1">Tiles & Granites</span>
+                        <span class="badge rounded-pill text-bg-light fs-6 p-3 m-1">Tiles & Granites</span>
                     </div>
-                </div>
-
-                <!-- Google Map Link -->
-                <div class="mb-3">
-                    <div class="form-floating">
-                        <input type="text" id="google_map_link" class="form-control" wire:model="google_map_link"
-                            placeholder="Please enter your Google Map link">
-                        <label for="google_map_link">Google Map Link</label>
-                    </div>
-                    @error('google_map_link')
-                        <span class="text-danger">{{ $message }}</span>
-                    @enderror
-                </div>
-
-                <!-- Navigation Buttons -->
-                <div class="d-flex mt-5">
-                    <button class="btn btn-defualt" wire:click="prevStep">
-                        <svg width="35" height="35" viewBox="0 0 50 50" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <rect x="-1" y="1" width="47" height="47" rx="24"
-                                transform="matrix(-1 0 0 1 48 0)" fill="white" stroke="black" stroke-width="2" />
-                            <path
-                                d="M20.9123 25.0026L29.3342 16.5807C29.6206 16.2943 29.7591 15.9553 29.7495 15.5638C29.74 15.1723 29.592 14.8333 29.3055 14.5469C29.0191 14.2604 28.6801 14.1172 28.2886 14.1172C27.8971 14.1172 27.5581 14.2604 27.2717 14.5469L18.4774 23.3698C18.2482 23.599 18.0764 23.8568 17.9618 24.1432C17.8472 24.4297 17.7899 24.7161 17.7899 25.0026C17.7899 25.2891 17.8472 25.5755 17.9618 25.862C18.0764 26.1484 18.2482 26.4063 18.4774 26.6354L27.3003 35.4583C27.5868 35.7448 27.921 35.8832 28.3029 35.8737C28.6849 35.8642 29.0191 35.7161 29.3055 35.4297C29.592 35.1432 29.7352 34.8043 29.7352 34.4128C29.7352 34.0213 29.592 33.6823 29.3055 33.3958L20.9123 25.0026Z"
-                                fill="black" />
-                        </svg>
-
-                    </button>
-                    <button class="btn btn-dark btn-sm px-5 rounded-5" wire:click="nextStep"
-                        {{ $userType ? '' : 'disabled' }}>
-                        Next
-                    </button>
                 </div>
             @elseif($step == 4 && $userType === 'business')
                 <div class="row">
@@ -520,7 +466,7 @@
                 </div>
             @endif
         </div>
-        <div class="col-xl-3 col-lg-5 col-md-6 text-end d-none d-md-block">
+        <div class="col-xl-3 col-lg-4 col-md-6 text-end d-none d-md-block">
             <img src="{{ asset('assets/image/rectangle_3.png') }}" alt="Onboarding Image" class="img-fluid">
         </div>
     </div>
