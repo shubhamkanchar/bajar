7dfaf12a4077e37d16df95b1cafa3684fe3944fe
shubhamkanchar <shubhamkanchar687@gmail.com>
2025-11-22 17:41:04 +0530

    Layout fixed

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -87,37 +87,5 @@
     @livewireScripts
     @stack('scripts')
     @include('layouts.partials.notify')
-    <script>
-        Livewire.hook("request", ({
-            fail
-        }) => {
-            fail(async ({
-                status,
-                preventDefault,
-                retry
-            }) => {
-                if (status === 419) {
-                    preventDefault();
-
-                    const response = await fetch("/refresh-csrf", {
-                        method: "GET",
-                        headers: {
-                            "Accept": "application/json",
-                        },
-                        credentials: "same-origin",
-                    });
-
-                    const data = await response.json();
-                    const token = data.token;
-
-                    document.querySelector('meta[name="csrf-token"]').setAttribute("content", token);
-
-                    Livewire.csrfToken = token;
-                    retry();
-                }
-            });
-        });
-    </script>
 </body>
-
 </html>
